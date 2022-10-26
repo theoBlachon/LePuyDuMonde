f24d9352bfe22d4b302730321251fc32772b2c64
Korbeau <116091631+Korbeau@users.noreply.github.com>
2022-10-26 09:35:58 +0200

    Add files via upload

--- Pages_admin/reservation.php
+++ Pages_admin/reservation.php
@@ -1,6 +1,14 @@
 <?php
 $bdd = new 
 PDO('mysql:host=localhost;port=3306;dbname=lepuydumonde','root', '');
+$requete1='SELECT * FROM billet, tarif';
+$resultats1 = $bdd->query($requete1) ;
+$listeobjet1=$resultats1->fetchAll() ;
+$resultats1->closeCursor() ;
+$requete2='SELECT * FROM tarif';
+$resultats2 = $bdd->query($requete2) ;
+$listeobjet2=$resultats2->fetchAll() ;
+$resultats2->closeCursor() ;
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -15,19 +23,27 @@ PDO('mysql:host=localhost;port=3306;dbname=lepuydumonde','root', '');
 <title>Inscription</title>
 <link rel="icon" type="image/x-icon" href="" />
 <link rel="stylesheet" href="" type="text/css" media="screen" title="" />
-<script src="scriptjs/avertissement.js" type="text/javascript"></script>
 </head>
 
 <body>
-	<form method="POST" action="commande.php">
+	<form method="POST" action="">
 		Nom: <input type="name" step="any" name="nom" id="nom" required/>
 		<br>
 		Prénom: <input type="name" step="any" name="prenom" id="prenom" required/>
 		<br/>
 		Email: <input type="text" step="any" name="email" id="email" required/>
 		<br>
-		Numero de telephone: <input type="number" step="any" name="numero" id="numero" required/>
+		Numero de telephone: <input type="number_format" step="any" name="numero" id="numero" maxlength=10 required/>
 		<br/>
+		Tarifs:
+			<select name="id_billets">
+					<?php
+					$nbobjet=count($listeobjet2) ;
+					for($i=0;$i<$nbobjet;$i++){
+						echo ("<option value=".$listeobjet2[$i]["Id_tarif"].">".$listeobjet2[$i]["nom_tarif"]."</option>");
+					} 
+					?>
+			</select><br/>
 		<input type="submit" value="valider" id="reserv">
 	</form>		
 	<section>
@@ -35,17 +51,33 @@ PDO('mysql:host=localhost;port=3306;dbname=lepuydumonde','root', '');
 		if(isset($_POST["nom"]) && isset($_POST["prenom"])){
 			$nom=$bdd->quote($_POST["nom"]); 
 			$prenom=$bdd->quote($_POST["prenom"]);
-			$numero=$bdd->quote($_POST["email"]); 
-			$email=$bdd->quote($_POST["numero"]); 
-
-
+			$numero=$bdd->quote($_POST["numero"]); 
+			$email=$bdd->quote($_POST["email"]); 
 			$sql = 'INSERT INTO client (nom_client, prenom_client, telephone_client, mail_client) VALUES ('.$nom.','. $prenom.','.$numero.','. $email.')';
 			$nbmaj=$bdd->exec($sql);
-			if($nbmaj==1){
-				echo ('Enregistrement effectuer, bienvenue !');
-			}
 		}
 		?>
+	<section>
+			<?php
+			if(isset($_POST["id_billets"])){
+				$client='SELECT MAX(id_client) AS "id_client" FROM client';
+				$selection = $bdd->query($client) ;
+				$idclient=$selection->fetchAll() ;
+				$selection->closeCursor() ;
+				$id_concert=2;
+				$nbobjet=count($idclient) ;
+				for($i=0;$i<$nbobjet;$i++){
+					$id_client=$idclient[$i]["id_client"];
+				} 
+				$tarif=$bdd->quote($_POST["id_billets"]); 
+				$sql2 = 'INSERT INTO billet (id_client, id_concert, id_tarif) VALUES ('.$id_client.','.$id_concert.','. $tarif.')';
+				$nbmaj=$bdd->exec($sql2);
+				if($nbmaj==1){
+					echo ('Reservation effectuée');
+					echo("<a href='index.html'> Retourner à la page d'accueil</a>");
+				}
+			}
+			?>
 	</section>
 
 	<footer>	
